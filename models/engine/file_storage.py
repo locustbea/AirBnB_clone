fec01e0c5bc8138967bd9b6df3c3e38a812f9de4
locustbea <locustbea@yahoo.com>
2023-12-18 16:00:35 +0100

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,70 +1,64 @@
-#!usr/bin/python3
-
-""" File storage model """
+#!/usr/bin/python3
+""" Doc Here """
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
+# from models.base_model import BaseModel #avoid circular
 
 
-class FileStorage():
+class FileStorage:
+    """ doc doc """
 
-    """
-    FileStorage class
-    """
+            __file_path = "file.json"
+                __objects = {}
 
-    __file_path = "file.json"
-    __objects = {}
-    __models = {
-        'User': User,
-        'BaseModel': BaseModel,
-        'State': State,
-        'City': City,
-        'Amenity': Amenity,
-        'Place': Place,
-        'Review': Review
-    }
-    def all(self):
-        """
-        Returns all objects in BaseModel class representing format
-        """
-        return self.__objects
+                    def all(self):
+                        """ doc doc """
+                                        return FileStorage.__objects
 
-    def new(self, obj):
-        """
-        Add obj to objects
-        """
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+                                    def new(self, obj):
+                                        """ doc doc """
+                                                            id = obj.to_dict()["id"]
+                                                                    className = obj.to_dict()["__class__"]
+                                                                            keyName = className+"."+id
+                                                                                    FileStorage.__objects[keyName] = obj
 
-    def save(self);
+                                                                                        def save(self):
+                                                                                            """ doc doc """
+                                                                                                            filepath = FileStorage.__file_path
+                                                                                                                    data = dict(FileStorage.__objects)
+                                                                                                                            for key, value in data.items():
+                                                                                                                                data[key] = value.to_dict()
+                                                                                                                                                    with open(filepath, 'w') as f:
+                                                                                                                                                        json.dump(data, f)
 
-        """"
-        Serialize objects in __objects to json objects and
-        save them in file.json file format """
-        json_objs = {}
-        for key, val in self.__objects.items():
-            json_objs[key] = val.to_dict()
-        with open(self.__file_path, "w", encoding='utf-8') as f:
-            json.dump(json_objs, f, indent=4)
-
-    def reload(self);
-        """
-        Deserializes the JSON objects in file.json
-        """
-        try:
-            with open(self.__file_path, "r", encoding='utf-8') as f:
-                json_objs = json.load(f)
-
-            for key, val in json_objs.items():
-                constractor = val["__class__"]
-                if val["__class__"] in self.__models.keys():
-                    self.__objects[key] = self.__models[val["__class__"]](
-                        **val)
-        except FileNotFoundError:
-            pass
-        except Exception as e:
-            pass
+                                                                                                                                                                        def reload(self):
+                                                                                                                                                                            """ doc doc """
+                                                                                                                                                                                            filepath = FileStorage.__file_path
+                                                                                                                                                                                                    data = FileStorage.__objects
+                                                                                                                                                                                                            if os.path.exists(filepath):
+                                                                                                                                                                                                                try:
+                                                                                                                                                                                                                    with open(filepath) as f:
+                                                                                                                                                                                                                        for key, value in json.load(f).items():
+                                                                                                                                                                                                                            if "BaseModel" in key:
+                                                                                                                                                                                                                                data[key] = BaseModel(**value)
+                                                                                                                                                                                                                                                                                                                                                            if "User" in key:
+                                                                                                                                                                                                                                                                                                                                                                data[key] = User(**value)
+                                                                                                                                                                                                                                                                                                                                                                                                                    if "Place" in key:
+                                                                                                                                                                                                                                                                                                                                                                                                                        data[key] = Place(**value)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                            if "State" in key:
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                data[key] = State(**value)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    if "City" in key:
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        data[key] = City(**value)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            if "Amenity" in key:
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                data[key] = Amenity(**value)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    if "Review" in key:
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        data[key] = Review(**value)
+                                                                                                                                                                                                                except Exception:
+                                                                                                                                                                                                                    pass
